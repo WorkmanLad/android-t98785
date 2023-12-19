dc3d1bea7f234d9f5f0ade54b70d8c862fc01220
Louis <Workmanlad2@outlook.com>
2023-12-19 15:29:41 -0300

    Improve metronome ui.

--- app/src/main/java/com/example/metronome/ui/MetronomeScreen.kt
+++ app/src/main/java/com/example/metronome/ui/MetronomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.metronome.ui
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,11 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Circle
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.outlined.Circle
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -25,15 +30,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.metronome.R
-import com.example.metronome.ui.theme.MetronomeTheme
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
@@ -64,23 +68,25 @@ fun MetronomeScreen(modifier: Modifier = Modifier, viewModel: MetronomeViewModel
                 .padding(16.dp),
             textAlign = TextAlign.Center
         )
-
         Spacer(Modifier.height(16.dp))
+
         BeatsPerMinutes(
             bpm = uiState.bpm,
-            onMinusButton = { viewModel.onMinusButton() },
-            onPlusButton = { viewModel.onPlusButton() },
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(Modifier.height(150.dp))
-        PlayButton(
-            playing = uiState.playing,
-            onPlayButton = { viewModel.onPlayButton() }
-        )
-        Spacer(Modifier.height(16.dp))
+        Spacer(Modifier.height(180.dp))
+
         Circles(
             active = uiState.activeCircle,
             modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(280.dp))
+
+        PlayButton(
+            playing = uiState.playing,
+            onPlayButton = { viewModel.onPlayButton() },
+            onMinusButton = { viewModel.onMinusButton() },
+            onPlusButton = { viewModel.onPlusButton() }
         )
     }
 }
@@ -88,33 +94,18 @@ fun MetronomeScreen(modifier: Modifier = Modifier, viewModel: MetronomeViewModel
 @Composable
 private fun BeatsPerMinutes(
     bpm: Int,
-    onMinusButton: () -> Unit,
-    onPlusButton: () -> Unit,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(onClick = onMinusButton, Modifier.animateContentSize()) {
-                Text("-", style = MaterialTheme.typography.headlineLarge, fontSize = 36.sp)
-            }
-
-            Text(
-                text = "$bpm",
-                style = MaterialTheme.typography.displayLarge,
-                fontSize = 36.sp,
-                modifier = Modifier.padding(horizontal = 32.dp)
-            )
-
-            IconButton(onClick = onPlusButton, Modifier.animateContentSize()) {
-                Text(text = "+", style = MaterialTheme.typography.headlineLarge, fontSize = 36.sp)
-            }
-        }
+        Text(
+            text = "$bpm",
+            style = MaterialTheme.typography.displayLarge,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
 
         Text("beats per min", style = MaterialTheme.typography.labelLarge)
     }
@@ -124,13 +115,48 @@ private fun BeatsPerMinutes(
 private fun PlayButton(
     playing: Boolean,
     onPlayButton: () -> Unit,
+    onMinusButton: () -> Unit,
+    onPlusButton: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    IconButton(onClick = onPlayButton, modifier) {
-        if (!playing) {
-            Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "Play", Modifier.size(160.dp))
-        } else {
-            Icon(imageVector = Icons.Filled.Pause, contentDescription = "Pause", Modifier.size(160.dp))
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        IconButton(onClick = onMinusButton, Modifier.animateContentSize()) {
+            Text("-", style = MaterialTheme.typography.headlineLarge, fontSize = 36.sp)
+        }
+        Card(
+            shape = RoundedCornerShape(50.dp),
+            modifier = Modifier
+                .width(75.dp)
+                .height(75.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            )
+        ) {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                IconButton(onClick = onPlayButton) {
+                    if (!playing) {
+                        Icon(
+                            imageVector = Icons.Filled.PlayArrow,
+                            contentDescription = "Play",
+                            Modifier.size(160.dp)
+                        )
+                    } else {
+                        Icon(
+                            imageVector = Icons.Filled.Pause,
+                            contentDescription = "Pause",
+                            Modifier.size(160.dp)
+                        )
+                    }
+                }
+            }
+        }
+        IconButton(onClick = onPlusButton, Modifier.animateContentSize()) {
+            Text(text = "+", style = MaterialTheme.typography.headlineLarge, fontSize = 36.sp)
         }
     }
 }
@@ -148,13 +174,5 @@ private fun Circles(active: Int, modifier: Modifier = Modifier) {
                 Icon(imageVector = Icons.Outlined.Circle, contentDescription = null)
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun MetronomePreview() {
-    MetronomeTheme(darkTheme = true) {
-        MetronomeScreen(Modifier.fillMaxSize())
     }
 }
